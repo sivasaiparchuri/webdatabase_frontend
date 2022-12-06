d3d444c747edfc025eb7956ac9017de097bc647b
siva <sivasai.parchuri@gmail.com>
2022-12-06 17:28:27 -0500

    1st commit

---
+++ src/components/BookBox.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="bookbox">
+
+
+        
+
+        <div v-for="book in books" :key="book.id" >
+            <BookItem :book="book"/>
+        </div>
+
+    </div>
+    
+
+
+</template>
+
+<script>
+
+    import BookItem from "@/components/BookItem.vue";
+
+
+    export default{
+        name: 'BookBox',
+        components: {
+            BookItem
+        },
+        props:{
+            
+            books: Array
+        }
+    }
+
+</script>
+
+<style scoped>
+
+.bookbox{
+
+background-color: rgb(196, 231, 255);
+padding: 1em;
+color: gray;
+}
+
+</style>
---
+++ src/components/BookItem.vue
@@ -0,0 +1,70 @@
+<template>
+
+    <div>
+        <h1 id="meal"> {{book.mealname}}</h1>
+        <h2 id="breakfast"> {{book.type}}</h2>
+        <h2> Protein : {{book.protein}} gm</h2>
+        <h2> Carbohydrates : {{book.carbs}} gm</h2>
+        <h2> Totalkcal : {{book.totalkcal}} Kcal</h2>
+        
+    </div>
+
+</template>
+
+<script>
+
+
+
+
+export default{
+    name: 'BookItem',
+    props:{
+        book: Array
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+h2{
+    text-align: center;
+}
+
+h1{
+    text-align: center;
+}
+#meal{
+  background-color: rgb(238, 193, 96);
+  color: black;
+    padding: 1em;
+    text-align: center; 
+
+}
+#breakfast{
+  background-color: rgb(96, 174, 238);
+  color: black;
+    padding: 1em;
+    text-align: center; 
+
+}
+#body{
+  background-color: #e6ff00;
+}
+div{
+    background-color: #a1ff9e;
+    color: black;
+    padding: 1em;
+}
+
+.red{
+background-color: orangered !important;
+
+}
+.green{
+    color: white;
+}
+
+
+</style>
